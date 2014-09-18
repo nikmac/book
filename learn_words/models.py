bfe399a4e7a44ddbd28db0dc093bb4bb57815285
Niki Macomber <nikicrisler@gmail.com>
2014-09-18 11:08:44 -0700

    again

--- learn_words/models.py
+++ learn_words/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
+class Article(models.Model):
+    text = models.TextField()
+
+
+class Word(models.Model):
+    users = models.ManyToManyField(User, related_name='words')
+    articles = models.ManyToManyField(Article, related_name='words')
+    learned = models.BooleanField(default=False)
