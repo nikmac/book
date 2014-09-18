a3d8debf434d1f456cc46a9ed942e89acfef2a02
Niki Macomber <nikicrisler@gmail.com>
2014-09-18 11:47:00 -0700

    niki made urls, views, filled in templates

--- learn_words/views.py
+++ learn_words/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def article(request):
+    return render(request, 'article.html')
+
+def register(request):
+    # return render(request, 'register.html')
+    pass
+
+
